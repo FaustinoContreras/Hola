5f961689b6fe194fab7695ed6807a43c6104d2e7
Edgarr <Edgarr@workgrouup>
2016-04-14 03:20:32 +0200

    pruebas

--- hola/src/hola/Hola.java
+++ hola/src/hola/Hola.java
@@ -5,8 +5,9 @@ public class Hola {
 	public static void main(String[] args) {
 		System.out.println("Hola  Tino - JOANMI");
 		System.out.println("HOLAHOLA");
+		//prueba
 		
-
+		int i = 2;
 	}
 
 }
