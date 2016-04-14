b188ab86e98c4b9e9d9193452957b6fbfe3b16e6
edgarRobayo <timetower1@gmail.com>
2016-04-14 02:31:26 +0200

    Update Hola.java

--- hola/src/hola/Hola.java
+++ hola/src/hola/Hola.java
@@ -3,7 +3,7 @@ package hola;
 public class Hola {
 
 	public static void main(String[] args) {
-		System.out.println("Hola                   Tino - JOANMI");
+		System.out.println("Hola  Tino - JOANMI");
 		System.out.println("HOLAHOLA");
 		
 
